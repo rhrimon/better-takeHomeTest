3d8c9bcd6648cc702a8a839e2ed23657020cfba1
Rimon Hasan <rhrimon@gmail.com>
2021-02-02 20:48:40 -0500

    Expanded get tests

--- Chat User Tests/test_users_200.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def test_users_200():
-    url = 'http://localhost:8080/api/users'
-    response = requests.get(url)
-    
-    assert response.status_code == 200
---
+++ Chat User Tests/test_users_get_200_404.py
@@ -0,0 +1,13 @@
+import requests
+
+def test_users_200():
+    url = 'http://localhost:8080/api/users'
+    response = requests.get(url)
+    
+    assert response.status_code == 200
+
+def test_users_404():
+    url = 'http://localhost:8080/api/users/id'
+    response = requests.get(url)
+    
+    assert response.status_code == 404
